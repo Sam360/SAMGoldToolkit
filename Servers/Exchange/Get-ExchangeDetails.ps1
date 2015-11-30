93815471e99689dbf48474b021dc757cb177ac55
Jon Mulligan <jon.mulligan@unknown.com>
2015-11-30 17:22:19 +0000

    Remove funny character from help section of Exchange script

--- Servers/Exchange/Get-ExchangeDetails.ps1
+++ Servers/Exchange/Get-ExchangeDetails.ps1
@@ -15,13 +15,13 @@ Retrieves Exchange server, mail box, ActiveSync device and CAL information from 
 .DESCRIPTION
 The Get-ExchangeDetails script queries a single Exchange server and produces up to 5 CSV files
 	1)    ExchangeServerDetails.csv - One record per Exchange Server in the farm
-    2)    ExchangeMailBoxes.csv - One record per MailBox
+	2)    ExchangeMailBoxes.csv - One record per MailBox
 	3)    ExchangeDevices.csv - One record per ActiveSync device
 	4)    ExchangeCALs.csv - General CAL requirement details 
-    5)    ExchangeCALDetails.csv - Lists all servers and MailBoxes that require a license and 
+	5)    ExchangeCALDetails.csv - Lists all servers and MailBoxes that require a license and 
 	      the type of license required
 
-    Files are written to current working directory
+	Files are written to current working directory
 
 .PARAMETER Server 
 Host name of Exchange server to scan
@@ -48,10 +48,6 @@ server. However, a different embedded script can be selected manually. Allowed o
 By default the script collects Exchange server, mail box, ActiveSync device and CAL information from the 
 selected Exchange server. It's possible to collect subsets of the data. Possible options are...
 "AllData","ServerData","EntityData","UtilizationData","CALData"
-
-.EXAMPLE
-Get all guest, host and migration info from Hyper-V server 'Defiant'. 
-Get-HyperVVMList –HyperVServer Defiant
 
 .NOTES
 This script supports Exchange server 2007 onwards. There are some limitations on what data can be
